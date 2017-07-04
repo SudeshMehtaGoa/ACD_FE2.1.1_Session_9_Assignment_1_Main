9fb6d93a4431aaaa0e5a3e8bdc03cf3649a46093
Mehta Sudesh <Mehta@iazi.ch>
2017-07-04 06:35:09 +0200

    Session 9 Assignment 1

---
+++ src/app/component/student-component/student.component.ts
@@ -0,0 +1,65 @@
+/* Add Student Component */
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+
+/* interfaces */
+import { IStudentList, IStudentModel } from 'app/interface/student-list';
+import { ISubjectType } from 'app/interface/student-subject';
+
+/* services */
+import { StudentService } from 'app/service/Student.service';
+import { SubjectService } from 'app/service/Subject.service';
+import { LogService } from 'app/service/log.service';
+
+@Component({
+  selector: 'student-root',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css'],
+  providers: [StudentService, SubjectService, LogService]
+})
+
+export class StudentComponent implements OnInit {
+
+  title: string = 'Add Student';
+
+  SubjectTypeArray: ISubjectType[] = [];
+
+  StudentsArray: IStudentList[] = [];
+  StudentDetail: IStudentList;
+
+  StudentModel: IStudentModel;
+
+  constructor(private _StudentService: StudentService,
+    private _SubjectService: SubjectService,
+    private _logService: LogService) {
+  }
+
+  /**Define default values on ngOnInit function */
+  ngOnInit() {
+    this.StudentModel = {
+      studentName: null,
+      studentAddress: null,
+      studentSubject: null,
+      studentMarks: null
+    };
+
+    this.SubjectTypeArray = this._SubjectService.getSubjectType();
+  };
+  /* addStudent method linked to form ngSubmit */
+  addStudent(values) {
+    this.StudentDetail = {
+      studentName: values.studentName,
+      studentAddress: values.studentAddress,
+      studentSubject: values.studentSubject,
+      studentMarks: values.studentMarks
+    };
+    this._StudentService.addStudent(this.StudentDetail);
+    this.StudentsArray = this._StudentService.getStudentList();
+    this._logService.log();
+    console.log(this.StudentDetail);
+  };
+  /* Form reset */
+  resetForm(f) {
+    f.reset();
+  };
+}
+
